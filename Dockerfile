5e97f3dca1c72e6d783de46f2d52ceee61cfe012
becgie <tramhuuduc.vn@gmail.com>
2026-06-08 20:21:45 +0700

    clean redundant code

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM amazoncorretto:17.0.10-al2023
-VOLUME /tmp
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} learning-spring-app.jar
-ENTRYPOINT ["sh", "-c", "java ${JAVA_OPTS} -jar /learning-spring-app.jar"]
--- build_image.sh
+++
@@ -1,8 +0,0 @@
-docker stop spring_app_ctn
-docker rm spring_app_ctn
-docker rmi spring_app
-
-mvn clean package -DskipTests
-
-docker build -t spring_app .
-docker run --name spring_app_ctn -p 8080:8080 spring_app
